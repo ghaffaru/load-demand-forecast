33b163af370c25ce481eb2cede62141e4f22edc5
Ghaffaru <mudashiruagm@gmail.com>
2020-02-26 08:40:18 +0000

    write scheduler

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from joblib import load
 import pandas as pd
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-# import scheduler
+import scheduler
 
 app = Flask(__name__)
 CORS(app)
--- scheduler.py
+++ scheduler.py
@@ -3,35 +3,43 @@ import requests
 import datetime
 import json
 import models
+from app import db
 
 now = datetime.datetime.now()
 hour = now.hour
 day = now.day
 month = now.month
-
-# def save_predictions_hourly():
-#     print('hello')
-
-# scheduler = BackgroundScheduler()
-# scheduler.add_job(func=save_predictions_hourly, trigger='interval', seconds=1)
-# scheduler.start()
-
-weather = requests.get(
+year = now.year
+def save_predictions_hourly():
+    weather = requests.get(
     'http://api.openweathermap.org/data/2.5/weather?q=Ghana,sunyani&appid=e3311f6761891b3558c08b64e1a9bcf9'
-)
-temperature = round(weather.json()['main']['temp'] - 273.15, 2)
-humidity = weather.json()['main']['humidity']
+    )
+    temperature = round(weather.json()['main']['temp'] - 273.15, 2)
+    humidity = weather.json()['main']['humidity']
 
-if (temperature and humidity):
-    headers = {'content-type': 'application/json'}
-    prediction = requests.post('http://localhost:5000/api/predict/hourly',
-                               data=json.dumps({
-                                   'hour': hour,
-                                   'day': day,
-                                   'month': month,
-                                   'humidity': humidity,
-                                   'temperature': temperature
-                               }),
-                               headers=headers)
+    if (temperature and humidity):
+        headers = {'content-type': 'application/json'}
+        prediction = requests.post('http://localhost:5000/api/predict/hourly',
+                                data=json.dumps({
+                                    'hour': hour,
+                                    'day': day,
+                                    'month': month,
+                                    'humidity': humidity,
+                                    'temperature': temperature
+                                }),
+                                headers=headers)
 
-    print(round(float(prediction.json()['prediction']), 2))
+        pred = round(float(prediction.json()['prediction']), 2)
+
+        pred_store = models.HourlyPrediction(hour=hour, day=day, month=month, year=year, humdity=humidity, temperature=temperature, prediction=pred)
+
+        db.session.add(pred_store)
+
+        db.session.commit()
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(save_predictions_hourly, 'cron', minute=41, second=0)
+scheduler.start()
+
+
+
